53392bd9880b040234b5d610513a698b5e1a7b04
udit1567 <uditmaurya2006@gmail.com>
2026-01-28 16:29:24 +0530

    'c2'

---
+++ Data_Structures/Graph/unweighted_directed_graph.py
@@ -0,0 +1,11 @@
+V = [0,1,2,3,4]
+E = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 4), (2, 3), (3, 4)]
+UE = E + [ (j,i) for (i,j) in E] # each edge represented by two tuple (u,v) and (v,u)
+size = len(V)
+AList = {}
+# In dictionay AList, for example, AList[i] = [j,k] represent two edge from i to j and i to k
+for i in range(size):
+    AList[i] = []
+for (i,j) in UE:
+    AList[i].append(j)
+print(AList)
